1de120939a6e6b84a7ebe21da80ae59a698902dc
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-24 18:23:55 +0900

    test

--- js/index.js
+++ js/index.js
@@ -40,11 +40,31 @@ addEventListener("DOMContentLoaded", async (event) => {
 
         console.log(musicList)
         for await (let music of musicList) {
-            document.querySelector('#player-list').innerHTML += `<div class="music"><div class="music-title">${music.split('.')[0].replace(/\_/gm, ' ')} <a href="./?a=${music.split('.')[0]}"><i class='bx bxs-right-arrow-circle'></i></a></div><div id="music-${music.split('.')[0]}"></div></div>`
+            document.querySelector('#player-list').innerHTML += `<div class="music"><div class="music-title"><a href="./?a=${music.split('.')[0]}">${music.split('.')[0].replace(/\_/gm, ' ')}</a> <i class='bx bx-play-circle' ></i></div><div id="music-${music.split('.')[0]}"></div></div>`
         }
 
         for await (let music of musicList) {
             new Function("let wavesurfer"+music.split('.')[0]+" = WaveSurfer.create({container: document.querySelector('#music-"+music.split('.')[0]+"'),waveColor: '#dddddd',progressColor: '#ffa358',url: './mp3/"+music+"',barWidth: 2,barGap: 1,barRadius: 2,});wavesurfer"+music.split('.')[0]+".on('interaction', () => {wavesurfer"+music.split('.')[0]+".playPause()})")()
+        }
+
+    } else if (audio == 'main') {
+
+        var result1 = await fetch(`https://api.github.com/repos/${USERNAME}/${REPONAME}/git/trees/main?recursive=1`)
+        var folderList = await result1.json()
+
+        var musicList = []
+        
+        for (let folder of folderList.tree) {
+            if (folder.path == 'mp3') {
+                var result2 = await fetch(folder.url)
+                var audioList = await result2.json()
+
+                for (let audio of audioList.tree) {
+                    if (audio.path.includes('.mp3')) {
+                        musicList.push(`${audio.path}`)
+                    }
+                }
+            }
         }
 
     } else {
@@ -59,6 +79,9 @@ addEventListener("DOMContentLoaded", async (event) => {
             barGap: 1,
             barRadius: 2,
         })
+
+        const audioContext = new AudioContext(); // 오디오 컨텍스트를 생성
+        const audioElement = document.querySelector(':host audio'); // HTML audio 요소 가져오기
 
         wavesurfer.on('interaction', () => {wavesurfer.playPause()})
 
